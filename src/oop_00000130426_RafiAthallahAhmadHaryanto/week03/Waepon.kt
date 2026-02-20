1ea74cc3e3e30ff91dee4032a5b0f28622d270f1
Rap <tinydiamondzgt@gmail.com>
2026-02-20 16:41:49 +0700

    week03: (task 1) create Weapon class with validation

---
+++ src/oop_00000130426_RafiAthallahAhmadHaryanto/week03/Waepon.kt
@@ -0,0 +1,33 @@
+package oop_00000130426_RafiAthallahAhmadHaryanto.week03
+
+import java.util.Scanner
+
+class Weapon (val name: String){
+    var damage: Int = 0
+        set(value) {
+            if(value < 0) {
+                println("WARNING: Do not input negative number!")
+                field = value
+            }else if(value > 1000){
+                field = 1000
+            }else {
+                field = value
+            }
+        }
+    var tier: String = ""
+        get(){
+            if(damage > 800) {
+                return "Legendary"
+            }else if(damage > 500) {
+                return "Epic"
+            }else return "Common"
+        }
+
+    fun printStat() {
+        println("=== WEAPON STATUS ===")
+        println("Weapon Name: $name")
+        println("Weapon Damage: $damage")
+        println("Weapon Tier: $tier")
+    }
+}
+
