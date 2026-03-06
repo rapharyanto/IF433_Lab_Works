cdb094c2a6ee84894eee9d3e955a8c3f48e8d4ea
Rap <tinydiamondzgt@gmail.com>
2026-03-06 16:51:50 +0700

    week05: (task 2) implement payment polymorphism

---
+++ src/oop_00000130426_RafiAthallahAhmadHaryanto/week05/PaymentMethodMain.kt
@@ -0,0 +1,28 @@
+package oop_00000130426_RafiAthallahAhmadHaryanto.week05
+
+import java.util.Scanner
+
+fun main(){
+    val scanner = Scanner(System.`in`)
+    print("Please input your account name: ")
+    val accName = scanner.nextLine()
+
+    val CC = CreditCard(accName, 100000.0)
+    val EW = EWallet(accName, 50000.0)
+    val ListOfPaymentMethod: List<PaymentMethod> = listOf(CC, EW)
+    println("\n=== USER DATA ===")
+    println("Account Name: $accName")
+
+    println("\n=== PAYMENT METHOD ===")
+    for(listPM in ListOfPaymentMethod) {
+        when(listPM) {
+            is CreditCard -> {
+                println("1. Payment with Credit Card")
+                listPM.processPayment(75000.0)
+            }
+            is EWallet -> {
+                listPM.processPayment(75000.0)
+            }
+        }
+    }
+}
