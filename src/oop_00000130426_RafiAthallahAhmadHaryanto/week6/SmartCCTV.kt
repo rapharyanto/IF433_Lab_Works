6a40be621e7ac6a3c5975308d40111f2bf8aa781
Rap <tinydiamondzgt@gmail.com>
2026-03-13 16:02:54 +0700

    week06: (task) create SmartCCTV with compound interfaces

---
+++ src/oop_00000130426_RafiAthallahAhmadHaryanto/week6/SmartCCTV.kt
@@ -0,0 +1,19 @@
+package oop_00000130426_RafiAthallahAhmadHaryanto.week6
+
+class SmartCCTV(override val id: String, override val name: String): SmartDevice, Switchable, Recordable {
+    override fun startRecord() {
+        println("Record started.")
+    }
+    override fun stopRecord() {
+        println("Record stopped.")
+    }
+
+    override fun turnOn() {
+        println("CCTV has been turned on.")
+        startRecord()
+    }
+    override fun turnOff() {
+        println("CCTV has been turned off.")
+        stopRecord()
+    }
+}
