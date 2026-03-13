e6e955f71410301fe753dee0b5a73f2dc2eef220
rapharyanto <tinydiamondzgt@gmail.com>
2026-03-13 11:11:17 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000130426_RafiAthallahAhmadHaryanto/week6/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_00000130426_RafiAthallahAhmadHaryanto.week6
+
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan.") }
+}
