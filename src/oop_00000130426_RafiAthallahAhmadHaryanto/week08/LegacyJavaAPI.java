a9d67ac5448669773ec61b0e12ff86181a693f71
Rap <tinydiamondzgt@gmail.com>
2026-04-17 15:09:11 +0700

    week08: create LegacyJavaAPI class in Java

---
+++ src/oop_00000130426_RafiAthallahAhmadHaryanto/week08/LegacyJavaAPI.java
@@ -0,0 +1,8 @@
+package oop_00000130426_RafiAthallahAhmadHaryanto.week08
+
+public class LegacyJavaAPI {
+    // Fungsi ini ditulis dalam Java. Kotlin tidak tahu apakah kembaliannya Nullable.
+    public static String fetchServerStatus() {
+        return "SERVER_ONLINE_200_OK";
+    }
+}
