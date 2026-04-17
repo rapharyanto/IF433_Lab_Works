0bdbb88a217c4219a07088f63c692c47c35a24d9
Rap <tinydiamondzgt@gmail.com>
2026-04-17 13:33:33 +0700

    week08: create basic UserProfile with nullable fields

---
+++ src/oop_00000130426_RafiAthallahAhmadHaryanto/week08/UserProfile.kt
@@ -0,0 +1,7 @@
+package oop_00000130426_RafiAthallahAhmadHaryanto.week08
+
+class UserProfile (
+    val name: String,
+    val email: String?,
+    val phone: String? = null //Default argument null
+)
