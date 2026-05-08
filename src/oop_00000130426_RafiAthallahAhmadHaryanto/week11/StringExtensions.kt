560d04931709cf0e17eae9a4b45faa6dc97d15dd
Rap <tinydiamondzgt@gmail.com>
2026-05-08 09:35:48 +0700

    week11: create nullable receiver extension

--- src/oop_00000130426_RafiAthallahAhmadHaryanto/week11/StringExtensions.kt
+++ src/oop_00000130426_RafiAthallahAhmadHaryanto/week11/StringExtensions.kt
@@ -5,3 +5,8 @@ fun String.addGreeting(): String { return "Hello, $this" }
 
 // Extension function dengan parameter
 fun String.repeatTimes(n: Int): String { return this.repeat(n) }
+
+fun String?.isNullorEmptyCustom(): Boolean {
+    // 'this' bisa bernilai null, jadi harus ditangani
+    return this == null || this.isEmpty()
+}
