62c3bac339c6a6967a6f8a14015f6a1117281ab8
Rap <tinydiamondzgt@gmail.com>
2026-05-08 09:18:27 +0700

    week10: (task) populate coin repository

--- src/oop_00000130426_RafiAthallahAhmadHaryanto/week10/CryptoDashboard.kt
+++ src/oop_00000130426_RafiAthallahAhmadHaryanto/week10/CryptoDashboard.kt
@@ -2,4 +2,14 @@ package oop_00000130426_RafiAthallahAhmadHaryanto.week10
 
 fun main() {
     val coinRepo = WalletRepository <Coin>()
+
+    coinRepo.add(Coin("BTC", 0.75))
+    coinRepo.add(Coin("ETH", 2.5))
+    coinRepo.add(Coin("USDT", 1500.0))
+
+    println("=== Wallet Coins ===")
+
+    coinRepo.getAll().forEach {
+        println("${it.name} : ${it.balance}")
+    }
 }
