54705176f5407ac72fd8fec61cf1fae57da34dec
Rap <tinydiamondzgt@gmail.com>
2026-05-15 08:53:39 +0700

    week12: (task) implement food stock validation with throw

--- src/oop_00000130426_RafiAthallahAhmadHaryanto/week12/SmartFeeder.kt
+++ src/oop_00000130426_RafiAthallahAhmadHaryanto/week12/SmartFeeder.kt
@@ -12,5 +12,10 @@ fun dispenseKibble(
     if (isJammed) {
         throw DispenserJamException()
     }
+    if (requestedGram > availableGram) {
+        throw FoodEmptyException(requestedGram, availableGram)
+    }
+
+    println("Kibble berhasil dikeluarkan!")
     return availableGram - requestedGram
 }
