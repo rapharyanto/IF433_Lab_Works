64331cde33f2685eae17919a990fa4597d2da46d
Rap <tinydiamondzgt@gmail.com>
2026-05-15 08:59:04 +0700

    week12: (task) handle feeding success using onSuccess

--- src/oop_00000130426_RafiAthallahAhmadHaryanto/week12/SmartFeeder.kt
+++ src/oop_00000130426_RafiAthallahAhmadHaryanto/week12/SmartFeeder.kt
@@ -43,5 +43,8 @@ fun main() {
             availableGram = 1000,
             isJammed = false
         )
+    }.onSuccess { newStock ->
+        currentKibbleStock = newStock
+        println("Makan sore sukses! Sisa stok kibble: $currentKibbleStock gr")
     }
 }
